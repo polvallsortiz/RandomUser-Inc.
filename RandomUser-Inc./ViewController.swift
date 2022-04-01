a993d27e10c2dab63c2fc6277b55ab81ba441df2
Pol Valls Ortiz <vallsortizpol@gmail.com>
2022-04-01 13:50:49 +0200

    Initial Commit

---
+++ RandomUser-Inc./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser-Inc.
+//
+//  Created by Pol Valls Ortiz on 1/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
